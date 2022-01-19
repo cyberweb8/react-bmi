a09a20418ee9492491468d63eecca5e679b79570
cyberweb8 <abdumalikshareef@gmail.com>
2022-01-20 00:42:50 +0500

    selectOption method and setting default unit on mounting

--- src/components/BMICalc.js
+++ src/components/BMICalc.js
@@ -1,8 +1,30 @@
-import React from 'react';
+import { useState, useEffect } from 'react';
 import FormInput from './FormInput';
 
 const BMICalc = () => {
+  const [unit, setUnit] = useState('');
+  const [heightUnit, setHeightUnit] = useState('');
+  const [weightUnit, setWeightUnit] = useState('');
+
+  useEffect(() => {
+    if (unit === '') {
+      setUnit('Metric');
+      setHeightUnit('cm');
+      setWeightUnit('kg');
+    }
+  }, [unit]);
   const onInputChange = (e) => {};
+
+  const onSelectOption = (e) => {
+    setUnit(e.target.value);
+    if (e.target.value === 'Metric') {
+      setHeightUnit('cm');
+      setWeightUnit('kg');
+    } else {
+      setHeightUnit('ft');
+      setWeightUnit('lbs');
+    }
+  };
   return (
     <>
       <div className="bmi-inputs">
@@ -10,7 +32,11 @@ const BMICalc = () => {
           <div>
             <span className="label-unit">Unit</span>
             <div className="unit">
-              <select name="unit" value="">
+              <select
+                name="unit"
+                value={unit}
+                onChange={onSelectOption}
+                className="form-control form-control-sm">
                 <option value="Metric">Metric</option>
                 <option value="Imperial">Imperial</option>
               </select>
@@ -20,23 +46,26 @@ const BMICalc = () => {
           <FormInput
             type="text"
             name="heightCount"
-            title={`Height (cm)`}
+            title={`Height (${heightUnit})`}
             value=""
             onChange={onInputChange}
           />
-
-          <FormInput
-            type="text"
-            name="inchesCount"
-            title={`Height (in)`}
-            value=""
-            onChange={onInputChange}
-          />
+          {unit === 'Imperial' ? (
+            <FormInput
+              type="text"
+              name="inchesCount"
+              title={`Height (in)`}
+              value=""
+              onChange={onInputChange}
+            />
+          ) : (
+            ''
+          )}
 
           <FormInput
             type="text"
             name="weightCount"
-            title={`Weight (kg)`}
+            title={`Weight (${weightUnit})`}
             value=""
             onChange={onInputChange}
           />
